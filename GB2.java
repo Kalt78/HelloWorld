0a361cf13bd5509531cd59fa77ac36f9d5b9e6a9
Andrew <dotter811@gmail.com>
2017-05-16 22:36:23 +0300

    HW

---
+++ GB2.java
@@ -0,0 +1,12 @@
+public class GB2 {
+	public static void main(String[] args) {
+		byte a = 10;
+		short b = 1010;
+		int c = 1000000;
+		long d = 10000L;
+		float e = 100.1f;
+		double f = 10.12d;
+		boolean g = true;
+		char h = 'A';
+	}
+}
